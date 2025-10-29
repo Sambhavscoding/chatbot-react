79236aab0a4c968656ca1fa75e93fb1a45e13915
Sambhav Prasad <sambhavjaiswal339@gmail.com>
2025-10-29 16:54:40 +0530

    added orders and tracking page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,8 @@ import {Routes, Route} from "react-router";
 import { HomePage } from './pages/homepage.jsx'
 import { CheckoutPage } from './pages/CheckoutPage.jsx' 
 import './App.css'
+import OrdersPage from "./pages/OrdersPage.jsx";
+import { TrackingPage } from "./pages/TrackingPage.jsx";
 
 function App() {
 
@@ -9,6 +11,9 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage/>} />
+       <Route path="orders" element={<OrdersPage/>} />
+
+       <Route path="tracking" element={<TrackingPage/>} />
     </Routes>
       
   )
